3542f77e0662a4a301db03b2dacedb0a52d0fd0d
Brianna Stephens <briannaastephens24@gmail.com>
2025-03-21 06:50:32 -0600

    added chead day as a focus goal

--- db/seed.cjs
+++ db/seed.cjs
@@ -156,6 +156,7 @@ const syncAndSeed = async() => {
   await createMeal(`Philly Cheesesteak Stuffed Peppers`, `protein`, 450, `swollpatrol`);
   await createMeal(`Beef Pad Thai`, `lowcal`, 470, `quackracer6`);
   await createMeal(`Sheet Pan Steak, Potatoes & Asparagus`, `lowcal`, 490, `joecallahan`);
+  await createMeal(`Oreos`, `Cheat Day!`, 650, 'bad.influence')
 
   await createProfile(`test`, `password`, `Test User`, 64, 130, 85, `female`);
   await createProfile(`gymyoga`, `meditate`, `Jim Yoga`, 72, 175, 40, `male`);
